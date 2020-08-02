7fc896b60e2d91fc7fdb78f55ee3688493cd9f7f
Ajay <53959804+ajay-02@users.noreply.github.com>
2020-08-02 20:53:52 +0530

    Update l3_p1_1.py

--- level 3/l3_p1_1.py
+++ level 3/l3_p1_1.py
@@ -1,5 +1,5 @@
 n=int(input())
 fact=[1,0]
 for i in range(2,n+1):
-  fact.append(((i-1)*((fact[i-1]+fact[i-2])%1000000007))%1000000007)
+  fact.append(((i-1)*((fact[i-1]+fact[i-2])%100000007))%100000007)
 print(fact[n])
